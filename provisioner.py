4a230d06532c325f375e4feb3934e483dc28757b
govorges <gio@openbroadcast.cz>
2025-01-17 15:01:14 -0500

    Update provisioner.py

--- provisioner.py
+++ provisioner.py
@@ -1,10 +1,15 @@
-print(__import__('json').dumps({key: { 
-    subKey: str(subValue) for subKey, subValue in value.items()
-} if not isinstance(value, str) else value for key, value in {
-    config.name: config.__dict__ for config in __import__('configurations').Loading.ListServerConfigurations()
-}.items()}, indent=2) if __import__("dotenv").load_dotenv() and __import__("os").environ.get('CLIENT_TOKEN') is not None \
-else "CLIENT_TOKEN was not set in the environment")
-# Obligatory overly-long-python-comprehension-for-fun. This file is still a WIP.
+from hcloud import Client
+from hcloud.floating_ips.client import BoundFloatingIP
 
+from dotenv import load_dotenv
+from os import environ
 
+load_dotenv()
+assert environ.get('CLIENT_TOKEN') is not None, \
+    "CLIENT_TOKEN was not set in the environment."
+CLIENT_TOKEN = environ['CLIENT_TOKEN']
 
+client = Client(token = CLIENT_TOKEN, application_name = "server-provisioning.provisioner")
+
+floating_ip_addresses: list[BoundFloatingIP] = client.floating_ips.get_all()
+print([address.ip for address in floating_ip_addresses])
