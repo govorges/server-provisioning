3f56eefc0a32856e155a23c266e54a80910583de
govorges <gio@openbroadcast.cz>
2025-01-16 20:57:39 -0500

    Configuration name set to __name__

--- configurations/example.py
+++ configurations/example.py
@@ -3,8 +3,9 @@ from hcloud.server_types import ServerType
 
 from ._configuration import Configuration
 
+CONFIGURATION_NAME = __name__ # Optional
 SERVER_CONFIGURATION = Configuration( # A file's server configuration must be named SERVER_CONFIGURATION.
-    name = "example-server",
-    server_type = ServerType(name="example"),
+    name = CONFIGURATION_NAME,
+    server_type = ServerType(name=CONFIGURATION_NAME),
     image = Image(name="ubuntu-22.04")
 )
